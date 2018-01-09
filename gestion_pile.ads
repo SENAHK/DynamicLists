0337807d208f6baf4bda51d6355c7360e0affe63
senahk <senahk94@gmail.com>
2018-01-09 14:18:19 +0100

    update gestion_pile.ads

--- gestion_pile.ads
+++ gestion_pile.ads
@@ -2,29 +2,108 @@ generic
    type T_Info is private;
 
 package gestion_pile is
-   type T_Queue is limited private;
-   -- Interface
-   function Vide(Q : T_Queue) return Boolean;
-   function Tete(Q : T_Queue) return T_Info;
-   function Queue(Q : T_Queue) return T_Info;
-   procedure Inserer(Q   : in out T_Queue;
-                     Val : in     T_Info);
-   procedure Extraire(Q   : in out T_Queue;
-                      Val :    out T_Info);
-   -- Exception
-   QUEUE_VIDE : exception;
+   type T_Pile is limited private;
+   --
+   -- Exceptions
+   --
+   Pile_Vide : exception;
 
+   --
+   -- Fonctionnalités d'une pile (fonctions et procédures)
+   --
+   -- Cette fonction retourne vrai si la pile <Pile> est vide et faux sinon
+   function Vide(Pile : T_Pile) return boolean;
+
+   -- Cette fonction retourne vrai si la pile <Pile> a atteint sa capacité maximale
+   -- et faux sinon
+   function Sommet(Pile : T_Pile) return T_Info;
+
+   -- Cette fonction empile la valeur <Info> sur la pile <Pile>
+   -- L'exception <Pile_Pleine> est levée si la pile <Pile> a atteint sa capacité maximale
+   procedure Empiler(Pile : in out T_Pile; Info : in T_Info);
+
+   -- Cette fonction retire la valeur stockée au sommet de la pile <Pile>
+   -- et la stocke dans <Info>
+   -- L'exception <Pile_Vide> est levée si la pile <Pile> est vide
+   procedure Depiler(Pile : in out T_Pile; Info : out T_Info);
+
+--
+-- Implémentation de la structure de pile
+--
 private
-   -- Structure des chainons de la queue
    type T_Element;
-   type T_Lien is access T_Element;
-   type T_Element is record
-      Valeur  : T_Info;
-      Suivant : T_Lien;
+   type T_Ptr_Element is access T_Element;
+   type T_Element is
+      record
+         Info    : T_Info;
+         Suivant : T_Ptr_Element := null;
+      end record;
+   type T_Pile is record
+      Sommet : T_Ptr_Element := null;
    end record;
-   -- Structure de queue
-   type T_Queue is record
-      Tete  : T_Lien;
-      Queue : T_Lien;
-   end record;
+
 end gestion_pile;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+generic
+   type T_Info is private;
+
+package Gestion_Pile_Dyn is
+   type T_Pile is limited private;
+   --
+   -- Exceptions
+   --
+   Pile_Vide : exception;
+
+   --
+   -- Fonctionnalités d'une pile (fonctions et procédures)
+   --
+   -- Cette fonction retourne vrai si la pile <Pile> est vide et faux sinon
+   function Vide(Pile : T_Pile) return boolean;
+   -- Cette fonction retourne vrai si la pile <Pile> a atteint sa capacité maximale
+   -- et faux sinon
+   function Sommet(Pile : T_Pile) return T_Info;
+   -- Cette fonction empile la valeur <Info> sur la pile <Pile>
+   -- L'exception <Pile_Pleine> est levée si la pile <Pile> a atteint sa capacité maximale
+   procedure Empiler(Pile : in out T_Pile; Info : in T_Info);
+   -- Cette fonction retire la valeur stockée au sommet de la pile <Pile>
+   -- et la stocke dans <Info>
+   -- L'exception <Pile_Vide> est levée si la pile <Pile> est vide
+   procedure Depiler(Pile : in out T_Pile; Info : out T_Info);
+
+--
+-- Implémentation de la structure de pile
+--
+private
+   type T_Element;
+   type T_Ptr_Element is access T_Element;
+   type T_Element is
+      record
+         Info    : T_Info;
+         Suivant : T_Ptr_Element := null;
+      end record;
+   type T_Pile is record
+      Sommet : T_Ptr_Element := null;
+   end record;
+
+end Gestion_Pile_Dyn;
