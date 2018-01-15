5aac05aaa8768f0592becc78e01b929ac380ad6c
senahk <senahk94@gmail.com>
2018-01-15 23:08:01 +0100

    functional version of sort by stack

--- tri2piles.adb
+++ tri2piles.adb
@@ -5,16 +5,65 @@ with Ada.Command_Line; use Ada.Command_Line;
 
 procedure tri2piles is
 
+   type T_Numbers is array(Natural range <>) of Integer;
 
-   P1: T_Stack;
-   P2: T_Stack;
+   procedure FillStack(Left, Right: in out T_Stack; Values: in T_Numbers) is
+      Nb, Popped: Integer;
+      Cpt: Natural := 1;
+   begin
+      -- Left stack should contain greater values
+      -- At first, push first val into it
+      Push(Left, Values(0));
+
+      -- Looping through the remaining elements
+      for I in 1..Values'Last loop
+         --Nb is the value to sort
+         Nb := Values(I);
+
+         -- While Nb > peak of left stack
+         while not Empty(Left) and then Nb > Peak(Left) loop
+            -- Pop the value from left, push it to right
+            Pop(Left, Popped);
+            Push(Right, Popped);
+         end loop;
+
+         -- While Nb < peak of right stack
+         while not Empty(Right) and then Nb < Peak(Right) loop
+            -- pop the value from right, push it to left
+            Pop(Right, Popped);
+            Push(Left, Popped);
+         end loop;
+
+         if not Empty(Right) and then Nb > Peak(Right) then
+            Push(Left, Nb);
+         end if;
+
+         if not Empty(Left) and then Nb > Peak(Left) then
+            Push(Left, Nb);
+         end if;
+
+      end loop;
+   end FillStack;
+
+   Left, Right: T_Stack;
    Val : Integer;
+   Values: T_Numbers(0..4) := (0 => 17,
+                               1 => 34,
+                               2 => 20,
+                               3 => 40,
+                               4 => 25  );
 begin
-   Push(P1, 3);
-   Put(Peak(P1));
-   Push(P1, 9);
-   Put(Peak(P1));
-   Pop(P1, Val);
-   Put(Peak(P1));
-   Put(Val);
+
+   FillStack(Left, Right, Values);
+   New_Line;
+   Put("Sorted:");
+   loop
+
+      New_Line;
+      Put(Peak(LEfT));
+      Pop(Left, Val);
+      New_Line;
+      exit when Empty(Left);
+   end loop;
+
 end tri2piles;
