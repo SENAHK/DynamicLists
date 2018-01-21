94141a3f58393aac241681e1e02c307178af7bf6
senahk <michael-arian.rms@eduge.ch>
2018-01-21 18:38:56 +0100

    arguments managements

--- dico.adb
+++ dico.adb
@@ -4,14 +4,7 @@ with Ada.Command_Line; use Ada.Command_Line;
 with gestion_liste; use gestion_liste;
 
 procedure dico is
-
-   -- Structure de liste
-   type T_Element;
-   type T_Lisa is access T_Element;
-   type T_Element is record
-      N : Integer;
-      Suivant : T_Lisa := null;
-   end record;
+   INVALID_ARGUMENTS: Exception;
 
    -- lecture d'un mot de caractères dans 'a'..'z' ou 'A'..'Z'
    procedure Read_Word(Fichier  : in File_Type;
@@ -29,79 +22,66 @@ procedure dico is
       end loop;
    end Read_Word;
 
-   -- Insère la valeur <Val> à la bonne position dans la liste triée <Liste>
-   procedure Inserer(Liste : in out T_Lisa; Val : in Integer) is
-      Tmp : T_Lisa := new T_Element'(Val,null);
-      Crt : T_Lisa := Liste;
-   begin
-      if Liste = null then
-         Liste := Tmp;
-      elsif Val <= Liste.all.N then
-         Tmp.all.Suivant := Liste;
-         Liste := Tmp;
-      else
-         while Crt.all.Suivant /= null
-               and then Val > Crt.all.Suivant.all.N loop
-            Crt := Crt.all.Suivant;
-         end loop;
-         Tmp.all.Suivant := Crt.all.Suivant;
-         Crt.all.Suivant := Tmp;
-      end if;
-end Inserer;
 
-      -- Affiche le contenu de la liste <Liste>
-   procedure Put(Liste : in T_Lisa) is
-      Crt : T_Lisa := Liste;
+
+
+   procedure FileToList(File: in string; List: in out T_List) is
+      Fichier : File_Type;
+      mot     : String(1..100) := (others => Ascii.Nul);
+      Lg : Natural := 0;
    begin
-      while Crt /= null loop
-         Put(Crt.all.N);
-         Crt := Crt.all.Suivant;
+      Open(Fichier,In_File, File);
+      while not End_Of_File(Fichier) loop
+         Read_Word(Fichier,mot,Lg);
+         if Lg > 0 then
+            Insert(List, mot(1..Lg));
+         end if;
       end loop;
-end Put;
-
-   Fichier : File_Type;
-   mot     : String(1..100) := (others => Ascii.Nul);
-   Lg : Natural := 0;
-
+      Close(Fichier);
+   end FileToList;
 
 
    L1, L2: T_List;
-   Nb: T_Lisa;
 begin
+   begin
+      if Argument_Count < 2 then
+         raise INVALID_ARGUMENTS;
 
---     Inserer(Nb, 4);
---     Inserer(Nb, 3);
---     Inserer(Nb, 2);
---     Put(Nb);
---        Open(Fichier,In_File,Argument(1));
---        while not End_Of_File(Fichier) loop
---           Read_Word(Fichier,mot,Lg);
---           if Lg > 0 then
---              Put_Line(mot(1..Lg));
---           end if;
---        end loop;
---        Close(Fichier);
+      else
+         if Argument_Count = 2 then
+            -- INSERT TO LIST
+            if Argument(1) = "-p" then
+               FileToList(Argument(2), L1);
+               Put(L1);
+            else
+               raise INVALID_ARGUMENTS;
+            end if;
+         end if;
 
-   Insert(L1, "il");
-   Insert(L1, "fait");
-   Insert(L1, "froid");
-   Insert(L1, "ce");
-   Insert(L1, "matin");
-   Insert(L1, "ce");
-   Insert(L1, "froid");
-   Insert(L1, "donne");
-   Insert(L1, "froid");
-   --Put(List);
-   Insert(L2, "pierre");
-   Insert(L2, "froid");
-   Insert(L2, "froid");
-   Insert(L2, "pierre");
-   Insert(L2, "pierre");
-   Insert(L2, "ce");
-   Insert(L2, "jeanne");
-   --Put(L1);
-   --Put( Intersect(L1, L2) );
-   --Put( Difference(L1, L2) );
-   Put( L1 + L2 );
-   --Put(Search(List, "matin"));
-end dico;
+         if Argument_Count = 3 then
+            FileToList(Argument(2), L1);
+
+            case Argument(1) is
+               when "-s" =>
+                  Put( Search(L1, Argument(3)) );
+               when "-x" =>
+                  Put( Delete(L1, Argument(3) ));
+               when "-i" =>
+                  FileToList(Argument(3), L2);
+                  Put( Intersect(L1, L2) );
+              when "-d" =>
+                  FileToList(Argument(3), L2);
+                  Put( Difference(L1, L2) );
+              when "-c" =>
+                  FileToList(Argument(3), L2);
+                  Put( L1 + L2 );
+
+               when others => raise INVALID_ARGUMENTS;
+            end case;
+         end if;
+
+      exception
+         when INVALID_ARGUMENTS => Put("Invalid Arguments");
+         when WORD_INEXISTANT => Put("Word not found in the list");
+      end;
+   end dico;
